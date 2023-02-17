7d9845704716075b69bb801432df7d5622c7a4b4
José Rodrigo Barrera <bar20807@uvg.edu.gt>
2023-02-16 23:23:14 -0600

    arreglos

--- Setup.py
+++ Setup.py
@@ -1,26 +1,43 @@
 import pathlib
 from setuptools import find_packages, setup
+from BayesianNetwork import __version__
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.0.1' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
-PACKAGE_NAME = 'Bayesian Network Library' #Debe coincidir con el nombre de la carpeta 
-AUTHOR = 'José Rodrigo Barrera García' #Modificar con vuestros datos
-AUTHOR_EMAIL = '' #Modificar con vuestros datos
-URL = '' #Modificar con vuestros datos
+VERSION = {}
+with open(HERE / "BayesianNetwork" / "version.py") as fp:
+    exec(fp.read(), VERSION)
+PACKAGE_NAME = "cool_bayesian_networks"
+AUTHOR = "José Rodrigo Barrera García"
+AUTHOR_EMAIL = "bar20807@uvg.edu.gt"
+URL = "https://github.com/bar20807/BayesianNetwork_IA"
 
-LICENSE = 'MIT' #Tipo de licencia
-DESCRIPTION = 'Librería para realizar cálculo de probabilidades de una red bayesiana' #Descripción corta
-LONG_DESCRIPTION = ("/BayesianNetwork" / "README.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
+LICENSE = "MIT"  # Tipo de licencia
+DESCRIPTION = (
+    "Permite el manejo y construccion de Redes Bayesianas"  # Descripción corta
+)
+
+LONG_DESCRIPTION = (HERE / "README.md").read_text(
+    encoding="utf-8"
+)  # Referencia al documento README con una descripción más elaborada
 LONG_DESC_TYPE = "text/markdown"
 
 
-#Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
-INSTALL_REQUIRES = []
+# Paquetes necesarios para que funcione la libreía. Se instalarán en el caso de que no esten instalados
+INSTALL_REQUIRES = ["build"]
+
+KEYWORDS = ["bayesian-networks", "artificial-intelligence", "machine-learning"]
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.10",
+]
 
 setup(
     name=PACKAGE_NAME,
-    version=VERSION,
+    version=VERSION["__version__"],
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type=LONG_DESC_TYPE,
@@ -30,5 +47,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     license=LICENSE,
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
 )
